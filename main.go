49772b8cd9eadfe67c589215698375a00bd37886
Conor Fleming <conorfleming03@gmail.com>
2022-11-18 19:47:57 -0800

    routing

--- main.go
+++ main.go
@@ -5,12 +5,31 @@ import (
 	"net/http"
 )
 
-func handlerfunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	switch path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFoundHandler()
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handlerfunc)
+	http.HandleFunc("/", pathHandler)
+	//http.HandleFunc("/contact", contactHandler)
 	fmt.Println("starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
