375bec36ccf4a76694511c2fc401636d2bde3f7a
Conor Fleming <conorfleming03@gmail.com>
2022-11-18 20:11:00 -0800

    messing with handlers

--- main.go
+++ main.go
@@ -5,14 +5,10 @@ import (
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
-}
+type Router struct{}
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	pathHandler(w, r)
 }
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,13 +19,23 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	case "/contact":
 		contactHandler(w, r)
 	default:
-		http.NotFoundHandler()
+		http.NotFound(w, r)
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
+}
+
 func main() {
-	http.HandleFunc("/", pathHandler)
+	var router Router
 	//http.HandleFunc("/contact", contactHandler)
 	fmt.Println("starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
