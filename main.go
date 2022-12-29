dc076cb4bfa9dbd0eac112c0207f45f4b2b7a4db
Conor-Fleming <conorfleming03@gmail.com>
2022-12-28 18:45:41 -0800

    adding faq page

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -18,9 +19,33 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
-		http.NotFound(w, r)
+		http.Error(w, "Page not found", http.StatusNotFound)
 	}
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<head>
+		<title>FAQ Page</title>
+	</head>
+  	<body>
+		<h1>Frequently Asked Questions</h1>
+  
+		<h2>What is this website about?</h2>
+		<p>This website is about providing information and resources on a variety of topics.</p>
+  
+		<h2>How can I contact the website owner?</h2>
+		<p>You can contact the website owner by sending an email to info@example.com.</p>
+  
+		<h2>Can I make suggestions for new content on the website?</h2>
+		<p>Yes, you can send your suggestions to info@example.com. We welcome feedback and new ideas for content.</p>
+  
+		<h2>Is this website free to use?</h2>
+		<p>Yes, this website is free to use for all visitors.</p>
+  </body>`)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,7 +60,6 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var router Router
-	//http.HandleFunc("/contact", contactHandler)
-	fmt.Println("starting the server on :3000...")
-	http.ListenAndServe(":3000", router)
+	fmt.Println("starting the server on :8080...")
+	log.Fatal(http.ListenAndServe(":8080", &router))
 }
