3c2ef592da0c28729d28708eafd809a9add06268
Conor-Fleming <conorfleming03@gmail.com>
2023-01-03 19:33:05 -0800

    setting up for tailwind

--- main.go
+++ main.go
@@ -18,14 +18,14 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	tmpl := views.Parse(templates.FS, "home.gohtml", "contact.gohtml", "faq.gohtml")
-	r.Get("/", controllers.StaticHandler(tmpl))
+	homeTmpl := views.Parse(templates.FS, "home.gohtml", "tailwind.gohtml")
+	r.Get("/", controllers.StaticHandler(homeTmpl))
 
-	tmpl = views.Parse(templates.FS, "contact.gohtml")
-	r.Get("/contact", controllers.StaticHandler(tmpl))
+	contactTmpl := views.Parse(templates.FS, "contact.gohtml", "tailwind.gohtml")
+	r.Get("/contact", controllers.StaticHandler(contactTmpl))
 
-	tmpl = views.Parse(templates.FS, "faq.gohtml")
-	r.Get("/faq", controllers.FAQ(tmpl))
+	faqTmpl := views.Parse(templates.FS, "faq.gohtml", "tailwind.gohtml")
+	r.Get("/faq", controllers.FAQ(faqTmpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
